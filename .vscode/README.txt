931d9b163344bc261cda8f1e835b43af72dd39aa
Jason McBride <jsononrails@gmail.com>
2017-03-17 15:44:48 -0400

    Initial Template Checkin
    React NodeJS Express Redux Boiler Template with WebPack and Babel

---
+++ .vscode/README.txt
@@ -0,0 +1,2 @@
+Tutorial Series
+https://www.youtube.com/watch?v=V08u9_noI9g
---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+app.use('/static', express.static(__dirname + '/public'));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'pug');
+
+app.get('/', function(req, res) {
+    res.render("index");
+});
+
+app.listen(3000, function() {
+    console.log('App is running on port 3000');
+});
